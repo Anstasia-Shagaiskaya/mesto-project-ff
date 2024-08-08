a6e7e151f97a2bf57a2850cad74650fcf090ecb6
Anastasia Shagaiskaya <anastasiashagaiskaya@yandex.ru>
2024-08-07 22:06:11 -0700

    mesto_project_1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
-
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
-
+function createCard(name, link, deleteCard) {
+    const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    // наполняем содержимым
+    placesItem.querySelector('.card__title').textContent = name;
+    const cardImage = placesItem.querySelector('.card__image');
+    cardImage.src = link;
+    cardImage.alt = ('На фотографии - ' + name);
+    const deleteButton = placesItem.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard); 
+    return placesItem;
+}
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const eventTarget = evt.target;
+    const placesItem = eventTarget.closest('.places__item');
+    placesItem.remove();
+}; 
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+placesList.append(createCard (element.name, element.link, deleteCard));
+  });
